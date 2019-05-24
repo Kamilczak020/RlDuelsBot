2f96c41f1d340a3b1e5f87ada244f240def67f60
Kamil Solecki <kamilczak020@gmail.com>
2019-05-24 13:26:43 +0200

    Registered new handlers with the bot, added database connection check

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 'use strict';
 import 'babel-polyfill';
+import { isNil } from 'lodash';
 import { Bot } from './core/bot';
 import { loadConfig } from './core/config';
 import { createDatabase } from './core/database';
@@ -8,18 +9,29 @@ import { createLogger } from './core/logger';
 import { EchoParser } from './parser/echoParser';
 import { SplitParser } from './parser/splitParser';
 import { EchoHandler } from './handler/echoHandler';
+import { KickHandler } from './handler/kickHandler';
+import { BanHandler } from './handler/banHandler';
 
 const logger = createLogger();
 const config = loadConfig('./build/config.yml');
 const database = createDatabase(config.database);
 const bot = new Bot(config, logger, database);
 
+database.sequelize.authenticate().then((errors) => {
+  if (!isNil(errors)) {
+    logger.error({ errors }, 'Failed to connect to database');
+    process.exit();
+  }
+})
+
 // register parsers
-bot.registerParser(new EchoParser(logger, config.parsers.echoParser));
-bot.registerParser(new SplitParser(logger, config.parsers.splitParser));
+bot.registerParser(EchoParser, config.parsers.echoParser);
+bot.registerParser(SplitParser, config.parsers.splitParser);
 
 // register handlers
-bot.registerHandler(new EchoHandler(logger, config.handlers.echoHandler));
+bot.registerHandler(EchoHandler, config.handlers.echoHandler);
+bot.registerHandler(KickHandler, config.handlers.kickHandler);
+bot.registerHandler(BanHandler, config.handlers.banHandler);
 
 if (process.argv[2] === 'sync') {
   try {
